4b4daf644d0dc2f8ff2d6685512069abba92bf54
Thomas Braun <thomasbraun@Thomass-MBP.fios-router.home>
2018-04-18 17:11:43 -0400

    Should fix a few bugs

--- Coin-complex/js/js.js
+++ Coin-complex/js/js.js
@@ -98,13 +98,8 @@ function check_if_in_view() {
 
 
 
-
-
-
-$(document).ready(function()
-{
-  var scrollTop = 0;
-  $(window).scroll(function(){
+var scrollTop = 0;
+$(window).scroll(function(){
     scrollTop = $(window).scrollTop();
     
     if (scrollTop >= 25) {
@@ -118,5 +113,3 @@ $(document).ready(function()
       $('#TopLogo').removeClass('CCxLogo');
     }  
   }); 
-
-});
